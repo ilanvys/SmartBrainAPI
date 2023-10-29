51130253ff185a9c3def6acb7bc0b0df1d13fb71
ilanvys <ilanvys@gmail.com>
2023-10-29 09:52:22 +0200

    Add Database

--- server.js
+++ server.js
@@ -1,28 +1,19 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs')
 const cors = require('cors');
+const knex = require('knex');
 
-let database = {
-  users: [
-    {
-      id: '123',
-      name: 'ilan',
-      email: 'ilanvys@email.com',
-      password: 'password1',
-      entries: 0,
-      joined: new Date()
-    },
-    {
-      id: '124',
-      name: 'yuval',
-      email: 'yuval@email.com',
-      password: 'yuvalon',
-      entries: 0,
-      joined: new Date()
-    }
-  ],
-  idCounter: 124
-}
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    port : 5432,
+    user : 'postgres',
+    password : 'test',
+    database : 'smart-brain'
+  }
+});
 
 const app = express();
 
@@ -33,63 +24,79 @@ app.get('/healthcheck', (req, res) => {
   res.status(200).send('App is running');
 })
 
+// TODO: remove this route
 app.get('/', (req, res) => {
-  res.send(database.users);
+  db.select('*').from('users')
+  .then(data => res.json(data))
 })
 
 app.post('/signin', (req, res) => {
-  if (req.body.email === database.users[0].email &&
-      req.body.password === database.users[0].password) {
-        res.json(database.users[0]);
-  } else {
-    res.status(400).json('error logging in')
-  }
+  return db.select('email', 'hash').from('login')
+  .where('email', '=', req.body.email)
+  .then(data => {
+    const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+    if (isValid) {
+      return db.select('*').from('users')
+      .where('email', '=', req.body.email)
+      .then(user => {
+        res.json(user[0])
+      })
+      .catch(err =>  res.status(400).json('Unable to get user'))
+    } else {
+      res.status(400).json('Wrong credentials')
+    }
+    })
+    .catch(err =>  res.status(400).json('Wrong credentials'))
 })
 
+// TODO: do express routes need a return? do db calls need to be returned?
 app.post('/register', (req, res) => {
   const { name, email, password } = req.body;
-  database.idCounter += 1;
-  database.users.push({
-    id: database.idCounter.toString(),
-    name: name,
-    email: email,
-    password: password,
-    entries: 0,
-    joined: new Date()
-  });
-  
-  res.json(database.users[database.users.length - 1]);
+  var hash = bcrypt.hashSync(password);
+  db.transaction(trx => {
+    trx.insert({ hash, email })
+    .into('login')
+    .returning('email')
+    .then(loginEmail => {
+      return trx('users')
+      .returning('*')
+      .insert({
+        email: loginEmail[0].email,
+        name: name,
+        joined: new Date()
+      })
+      .then(user => {
+        res.json(user[0]);
+      })
+    })
+    .then(trx.commit)
+    .catch(trx.rollback)
+  }).catch(err => res.status(400).json('Unable To Register'));
 })
 
 // TODO: Maybe improve that a tiny bit
 // TODO: extract repeated code to a function
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
-  let found = false;
-  database.users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
+  db.select('*').from('users').where({id})
+  .then(user => {
+    if (user.length) {
+      res.json(user[0]);
+    } else {
+      res.status(400).json('User Not Found')
     }
   })
-  if (!found) {
-    res.status(404).json("User Not Found");
-  }
+  .catch(err => res.status(404).json('Error getting user'));
 })
 
 app.put('/image', (req, res) => {
   const { id } = req.body;
-  let found = false;
-  database.users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
+  db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    res.json(entries[0].entries);
   })
-  if (!found) {
-    res.status(404).json("User Not Found");
-  }
 })
 
 app.listen(3000, () => {
