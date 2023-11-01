a56f10c9d2274bae5fa1105ce64a5f972dd93d37
ilanvys <ilanvys@gmail.com>
2023-11-01 14:49:16 +0200

    Add some security checks

--- controllers/register.js
+++ controllers/register.js
@@ -1,5 +1,8 @@
 const handleRegister = (req, res, db, bcrypt) => {
   const { name, email, password } = req.body;
+  if (!name || !email || !password) { //TODO: validate correct email 
+    return res.status(400).json('Incorrect form submission')
+  }
   var hash = bcrypt.hashSync(password);
   db.transaction(trx => {
     trx.insert({ hash, email })
